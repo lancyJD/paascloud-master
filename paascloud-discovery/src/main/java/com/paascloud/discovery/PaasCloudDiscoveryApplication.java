4ea3360f1a280d0565df0f3d1f55ba051606cde7
lancyJD <dengjiu2030@163.com>
2019-08-23 10:08:03 +0800

    test

--- paascloud-discovery/src/main/java/com/paascloud/discovery/PaasCloudDiscoveryApplication.java
+++ paascloud-discovery/src/main/java/com/paascloud/discovery/PaasCloudDiscoveryApplication.java
@@ -15,12 +15,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 /**
  * The class Paas cloud discovery application.
  *
  * @author paascloud.net@gmail.com
  */
+@RefreshScope
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
